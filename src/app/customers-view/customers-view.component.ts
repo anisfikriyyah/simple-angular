70d73244710fa1170c65967f97f9c2236b2b6261
Asrul Harahap <asrul@Asruls-MacBook-Pro.local>
2019-12-18 10:52:58 +0700

    Hook remove

--- src/app/customers-view/customers-view.component.ts
+++ src/app/customers-view/customers-view.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Customer} from '../models/customer';
 import {Observable} from 'rxjs';
 import {select, Store} from '@ngrx/store';
+import {CustomerRemove} from '../customer.actions';
 @Component({
   selector: 'app-customers-view',
   templateUrl: './customers-view.component.html',
@@ -11,5 +12,9 @@ export class CustomersViewComponent {
   customers: Observable<Customer[]>;
   constructor(private store: Store<{ customers: Customer[] }>) {
     this.customers = store.pipe(select('customers'));
+  }
+
+  removeCustomer(customerIndex) {
+    this.store.dispatch(new CustomerRemove(customerIndex));
   }
 }
